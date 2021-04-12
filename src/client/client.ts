739b0599ef1849b6a3bb47ce7d012fa24b511d9e
Jose Manuel <jmdcarq89@gmail.com>
2021-04-12 12:37:42 +0200

    envTexture

--- src/client/client.ts
+++ src/client/client.ts
@@ -39,12 +39,19 @@ const torusKnotGeometry: THREE.TorusKnotGeometry = new THREE.TorusKnotGeometry()
 
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial(); //{ color: 0x00ff00, wireframe: true })
 
-const texture = new THREE.TextureLoader().load("img/grid.png");
-material.map = texture;
-// const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
-// envTexture.mapping = THREE.CubeReflectionMapping
-// envTexture.mapping = THREE.CubeRefractionMapping
-// material.envMap = envTexture
+// const texture = new THREE.TextureLoader().load("img/grid.png");
+// material.map = texture;
+const envTexture = new THREE.CubeTextureLoader().load([
+  "img/px_50.png",
+  "img/nx_50.png",
+  "img/py_50.png",
+  "img/ny_50.png",
+  "img/pz_50.png",
+  "img/nz_50.png",
+]);
+envTexture.mapping = THREE.CubeReflectionMapping;
+envTexture.mapping = THREE.CubeRefractionMapping;
+material.envMap = envTexture;
 
 const cube: THREE.Mesh = new THREE.Mesh(boxGeometry, material);
 cube.position.x = 5;
